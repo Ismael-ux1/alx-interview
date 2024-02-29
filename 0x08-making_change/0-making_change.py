5dd2eb173bab7380947c7d56e2dc0b3ae68e532a
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-02-29 20:49:42 +0000

    Making Change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This function calculates the minimum number of coins,
+needed to make a given total.
+"""
+
+
+def makeChange(coins, total):
+    """
+    This function calculates the minimum number of coins,
+    needed to make a given total.
+    """
+    # Initialize the dp table
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    # Build the dp table
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    # Return the result
+    return dp[total] if dp[total] != float('inf') else -1
