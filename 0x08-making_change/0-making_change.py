b81c349bb946727f0099c744f78674145fd178bb
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-03-03 22:51:13 +0000

    updated def makeChange

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -19,7 +19,7 @@ def makeChange(coins, total):
 
     # Build the dp table
     for coin in coins:
-        for i in range(1, total + 1):
+        for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
     if dp[total] == float('inf'):
