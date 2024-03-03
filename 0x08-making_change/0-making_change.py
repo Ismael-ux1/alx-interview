183c96461a31719bcbd5cd674cdb730d7ccc4334
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-03-03 23:28:42 +0000

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,15 +13,16 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    coins.sort(reverse=True)
+    num_coins = 0
+    remaining_total = total
 
     for coin in coins:
-        for i in range(1, total + 1):
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+        while remaining_total >= coin:
+            remaining_total -= coin
+            num_coins += 1
 
-    if dp[total] == float('inf'):
-        return -1  # Cannot be met
+    if remaining_total == 0:
+        return num_coins
     else:
-        return dp[total]
+        return -1
